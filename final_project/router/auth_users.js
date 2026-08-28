854852831f1aaf1fab86d6c70959a1dfafd451ad
manuelp3110 <manuelp3110@gmail.com>
2026-08-28 00:49:20 -0400

    Task 8: add and modify book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -45,8 +45,22 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username;
+
+    if (!books[isbn]){
+        return res.status(404).json({message: "Book not found"});
+    }
+    if (!review) {
+        return res.status(400).json({message: "Review content is required"});
+    }
+    books[isbn].reviews[username] = review;
+    return res.status(200).json({
+        message: `Review for book ${isbn} added/updated by ${username}`,
+        reviews: books[isbn].reviews
+    });
+
 });
 
 module.exports.authenticated = regd_users;
